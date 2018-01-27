a5488113f849d7aac934725b087299be0bcf0554
Rogier <34269591+rogiertenhacken@users.noreply.github.com>
2018-01-27 22:22:19 +0100

    Add files via upload

---
+++ pwg.js
@@ -0,0 +1,15 @@
+const generator = () => {
+  let password = [];
+  const use = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@!$#%&*^?</>|+()§';
+
+  for (let i=0; i<12; i++)
+  password += use.charAt(Math.floor(Math.random() * use.length));
+
+   let newPassword = Array.from(password);
+   list = newPassword;
+   list = list.filter((x, i, a) => a.indexOf(x) == i)
+   savePassword = list.join('');
+
+  return savePassword;
+}
+console.log(generator());
